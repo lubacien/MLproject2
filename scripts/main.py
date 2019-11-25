338b345a4930f3ccfba9f0d5e0dacedf3454a528
Unknown <lucien.barret@gmail.com>
2019-11-25 16:16:51 +0100

    CAMARCHE chouettos
    yéé

--- scripts/main.py
+++ scripts/main.py
@@ -71,7 +71,7 @@ for i in range(1, 51):
 submission_filename = '../data/submission.csv'
 image_filenames = []
 for i in range(1, 51):
-    image_filename = '../data/submission/predictionim' + '%.3d' % i + '.png'
+    image_filename = '../data/submission/prediction' + '%.3d' % i + '.png'
     image_filenames.append(image_filename)
 
 masks_to_submission(submission_filename, *image_filenames)
