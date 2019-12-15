cb8cf6b5923931261e49eb38c640ed9af16870dc
Unknown <lucien.barret@gmail.com>
2019-12-15 14:13:38 +0100

    encore plus de loss

--- scripts/losses.py
+++ scripts/losses.py
@@ -7,8 +7,8 @@ def dice_coeff(y_true, y_pred):
     # Flatten
     y_true_f = tf.reshape(y_true, [-1])
     y_pred_f = tf.reshape(y_pred, [-1])
-    intersection = tf.reduce_sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    intersection = tf.reduce_sum(y_true_f * y_pred_f) #=truepositives
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth) #truepos+falseneg+truepos+false positives
     return score
 
 def dice_loss(y_true, y_pred):
@@ -47,6 +47,47 @@ def f1_score(y_true, y_pred):
     f1_score = 2 * (precision * recall) / (precision + recall)
     return f1_score
 
+def origf1_score(y_true, y_pred):
+
+    TP = tfK.sum(tfK.round(tfK.clip(y_true * y_pred, 0, 1)))
+    TPTN = tfK.sum(tfK.round(tfK.clip(y_pred, 0, 1)))
+    TPFN = tfK.sum(tfK.round(tfK.clip(y_true, 0, 1)))
+
+    # F1=0 when there is no true sample
+    if TPFN == 0:
+        return 0
+
+    # truepositives/truepositives+falsepositives
+    precision = TP / TPFP
+
+    # truepositives/truepositives+falsenegatives
+    recall = TP / TPFN
+
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
+
+
 def f1_loss(y_true,y_pred):
     return 1-f1_score(y_true,y_pred)
+
+def f1_score_noclip(y_true, y_pred):
+
+    TP = tfK.sum(tfK.round(y_true * y_pred, 0, 1))
+    TPTN = tfK.sum(tfK.round(y_pred, 0, 1))
+    TPFN = tfK.sum(tfK.round(y_true, 0, 1))
+
+    # F1=0 when there is no true sample
+    if TPFN == 0:
+        return 0
+
+    # truepositives/truepositives+falsepositives
+    precision = TP / TPFP
+
+    # truepositives/truepositives+falsenegatives
+    recall = TP / TPFN
+
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
 
